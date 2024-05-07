2232c08ed91e6887d6d3076d4b9703e59edf23c8
Melina Pashna <melinaapashna@gmail.com>
2024-05-07 12:56:23 -0700

    checking my commits

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ function App() {
   return (
     <>
       <h1 className='welcome'>Welcome To Canada</h1>
+      <h1 className='welcome'>Welcome To Canada</h1>
+      <h1 className='welcome'>Welcome To Canada</h1>
+      <h1 className='welcome'>Welcome To Canada</h1>
     </>
   )
 }
